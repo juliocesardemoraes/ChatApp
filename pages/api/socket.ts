ef736e3a3992f6c40a5c487a1e325f1f6d618ba9
julio cesar <jcmcf2012@gmail.com>
2022-07-31 10:18:04 -0300

    build - add socket connection

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,29 +1,29 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { Server } from "Socket.IO";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Server } from 'Socket.IO';
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 /**
  * Socket.IO Connection
- * @param req
- * @param res
+ * @param {NextApiRequest} req: Socket Io requisition
+ * @param {NextApiResponse} res: Socket IO response
  */
 export default (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (res.socket.server.io) {
-      console.log("Socket is already running");
+      console.log('Socket is already running');
     } else {
-      console.log("Socket is initializing");
+      console.log('Socket is initializing');
       const io = new Server(res.socket.server);
       res.socket.server.io = io;
 
-      io.on("connection", (socket) => {
-        socket.on("input-change", (msg) => {
-          socket.broadcast.emit("update-input", msg);
+      io.on('connection', (socket) => {
+        socket.on('input-change', (msg) => {
+          socket.broadcast.emit('update-input', msg);
         });
       });
     }
     res.end();
   } catch (err) {
-    console.log("ERROR", err);
+    console.log('ERROR', err);
   }
 };
